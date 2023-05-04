52054dd9285d70e57eca1ef2dfda534b2ca17346
pengyutao <pengyutao@suzhouyunque.com>
2023-05-04 18:17:13 +0800

    搭建商品分类

--- src/pages/product/category/UpdateForm.jsx
+++ src/pages/product/category/UpdateForm.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Form, Input} from 'antd'
 
 const UpdateForm = (props) => {
+
+  const categoryName = props.categoryName
+  const [form] = Form.useForm();
 
   const categoryNameRules = [
     {min: 2, message: '分类名称至少2位!'},
     {max: 12, message: '分类名称最多12位!'}
   ]
 
+  useEffect(() => {
+    form.setFieldsValue({categoryName})
+  }, [props])
+
   return (
-    <Form initialValues={{categoryName: props.categoryName}}>
+    <Form form={form}>
       <Form.Item name="categoryName" rules={categoryNameRules}>
         <Input placeholder="请输入分类名称"/>
       </Form.Item>
