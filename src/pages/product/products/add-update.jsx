d11df079e9194c43e54ae2147344ba8450de7cce
pengyutao <pengyutao@suzhouyunque.com>
2023-05-11 17:34:00 +0800

    搭建商品管理-上传图片

--- src/pages/product/products/add-update.jsx
+++ src/pages/product/products/add-update.jsx
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import {Card, Cascader, Upload, Button, Form, Input, InputNumber, message} from 'antd'
+import ImgCrop from 'antd-img-crop'
 
 import ArrowTitle from '../../../components/ArrowTitle'
 import {reqCategoryList} from '../../../api'
 import {useLocation} from 'react-router-dom'
+
+const {Item} = Form
 
 const ProductAddUpdate = () => {
 
@@ -12,15 +15,22 @@ const ProductAddUpdate = () => {
 
   const [isUpdate, setIsUpdate] = useState(false)
   const [product, setProduct] = useState({})
-
   const [categoryOptions, setCategoryOptions] = useState([])
+  const [fileList, setFileList] = useState([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+    }
+  ])
 
   useEffect(() => {
     getCategoryOptions().then(options => {
       setCategoryOptions(options)
     })
     initFormData()
-  },[])
+  }, [])
 
   const initFormData = () => {
     const product = location.state
@@ -38,20 +48,20 @@ const ProductAddUpdate = () => {
   }
 
   function buildOptionTree(list, parentId) {
-    const tree = [];
+    const tree = []
     for (const one of list) {
       if (one.parentId === parentId) {
-        const child = buildOptionTree(list, one.categoryId);
+        const child = buildOptionTree(list, one.categoryId)
         const node = {
           value: one.categoryId,
           label: one.categoryName,
           children: child,
           isLeaf: child.length <= 0
-        };
-        tree.push(node);
+        }
+        tree.push(node)
       }
     }
-    return tree;
+    return tree
   }
 
   const getCategoryOptions = async () => {
@@ -62,6 +72,24 @@ const ProductAddUpdate = () => {
       message.error(response.message).then()
       return []
     }
+  }
+
+  const onChange = ({fileList: newFileList}) => {
+    setFileList(newFileList)
+  }
+  const onPreview = async (file) => {
+    let src = file.url
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader()
+        reader.readAsDataURL(file.originFileObj)
+        reader.onload = () => resolve(reader.result)
+      })
+    }
+    const image = new Image()
+    image.src = src
+    const imgWindow = window.open(src)
+    imgWindow?.document.write(image.outerHTML)
   }
 
   const submit = async () => {
@@ -76,44 +104,73 @@ const ProductAddUpdate = () => {
   return (
     <Card title={<ArrowTitle>{isUpdate ? '修改商品' : '添加商品'}</ArrowTitle>}>
       <Form
-        labelCol={{span:3}}
-        wrapperCol={{span:6}}
+        labelCol={{span: 3}}
+        wrapperCol={{span: 8}}
         form={form}>
-        <Form.Item
-          label="商品名称" name="productName"
+
+        <Item
+          label="商品名称"
+          name="productName"
           rules={[
             {required: true, message: '商品名称不能为空'}
           ]}>
           <Input placeholder="请输入商品名称"/>
-        </Form.Item>
-        <Form.Item
-          label="商品描述" name="productDesc"
+        </Item>
+
+        <Item
+          label="商品描述"
+          name="productDesc"
           rules={[
             {required: true, message: '商品描述不能为空'}
           ]}>
-          <Input.TextArea placeholder="请输入商品描述" autosize={{minRows: 2}}/>
-        </Form.Item>
-        <Form.Item
+          <Input.TextArea
+            placeholder="请输入商品描述"
+            autosize={{minRows: 2}}/>
+        </Item>
+
+        <Item
           label="商品价格" name="price"
           rules={[
             {required: true, message: '价格不能为空'},
             {type: 'number', min: 0, message: '价格格式不正确'}
           ]}>
-          <InputNumber placeholder="请输入商品价格" addonAfter="元"/>
-        </Form.Item>
-        <Form.Item label="商品分类" name='categoryIds'>
+          <InputNumber
+            placeholder="请输入商品价格"
+            addonAfter="元"/>
+        </Item>
+
+        <Item
+          label="商品分类"
+          name="categoryIds"
+          rules={[
+            {required: true, message: '必须指定商品分类'}
+          ]}>
           <Cascader
+            placeholder="请指定商品分类"
             options={categoryOptions}/>
-        </Form.Item>
-        <Form.Item label="商品图片">
-          <div>商品图片</div>
-        </Form.Item>
-        <Form.Item label="商品详情">
+        </Item>
+
+        <Item label="商品图片">
+          <ImgCrop rotationSlider>
+            <Upload
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              listType="picture-card"
+              fileList={fileList}
+              onChange={onChange}
+              onPreview={onPreview}
+            >
+              {fileList.length < 5 && '+ Upload'}
+            </Upload>
+          </ImgCrop>
+        </Item>
+
+        <Item label="商品详情">
           <div>商品详情</div>
-        </Form.Item>
-        <Form.Item>
+        </Item>
+
+        <Item>
           <Button type="primary" onClick={submit}></Button>
-        </Form.Item>
+        </Item>
       </Form>
     </Card>
   )
