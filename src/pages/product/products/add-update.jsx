91dafcfc68754c0e63e1e71400ed4ff81006b640
pengyutao <pengyutao@suzhouyunque.com>
2023-05-18 16:04:56 +0800

    搭建商品管理-上传图片

--- src/pages/product/products/add-update.jsx
+++ src/pages/product/products/add-update.jsx
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react'
-import {Card, Cascader, Upload, Button, Form, Input, InputNumber, message} from 'antd'
-import ImgCrop from 'antd-img-crop'
+import {useLocation} from 'react-router-dom'
+import {Card, Cascader, Button, Form, Input, InputNumber, message} from 'antd'
 
 import ArrowTitle from '../../../components/ArrowTitle'
+import PictureWall from './picture-wall'
 import {reqCategoryList} from '../../../api'
-import {useLocation} from 'react-router-dom'
 
 const {Item} = Form
 
@@ -16,14 +16,6 @@ const ProductAddUpdate = () => {
   const [isUpdate, setIsUpdate] = useState(false)
   const [product, setProduct] = useState({})
   const [categoryOptions, setCategoryOptions] = useState([])
-  const [fileList, setFileList] = useState([
-    {
-      uid: '-1',
-      name: 'image.png',
-      status: 'done',
-      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
-    }
-  ])
 
   useEffect(() => {
     getCategoryOptions().then(options => {
@@ -74,24 +66,6 @@ const ProductAddUpdate = () => {
     }
   }
 
-  const onChange = ({fileList: newFileList}) => {
-    setFileList(newFileList)
-  }
-  const onPreview = async (file) => {
-    let src = file.url
-    if (!src) {
-      src = await new Promise((resolve) => {
-        const reader = new FileReader()
-        reader.readAsDataURL(file.originFileObj)
-        reader.onload = () => resolve(reader.result)
-      })
-    }
-    const image = new Image()
-    image.src = src
-    const imgWindow = window.open(src)
-    imgWindow?.document.write(image.outerHTML)
-  }
-
   const submit = async () => {
     try {
       const values = await form.validateFields()
@@ -129,7 +103,8 @@ const ProductAddUpdate = () => {
         </Item>
 
         <Item
-          label="商品价格" name="price"
+          label="商品价格"
+          name="price"
           rules={[
             {required: true, message: '价格不能为空'},
             {type: 'number', min: 0, message: '价格格式不正确'}
@@ -151,17 +126,7 @@ const ProductAddUpdate = () => {
         </Item>
 
         <Item label="商品图片">
-          <ImgCrop rotationSlider>
-            <Upload
-              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-              listType="picture-card"
-              fileList={fileList}
-              onChange={onChange}
-              onPreview={onPreview}
-            >
-              {fileList.length < 5 && '+ Upload'}
-            </Upload>
-          </ImgCrop>
+          <PictureWall/>
         </Item>
 
         <Item label="商品详情">
